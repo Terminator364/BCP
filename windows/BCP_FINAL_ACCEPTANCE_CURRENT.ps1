f9082f8b7341a045eff4f336e600bd660afb418c
Terminator364 <158576221+Terminator364@users.noreply.github.com>
2026-09-19 16:56:56 +0100

    Recovery: restore BCP 0.6 final acceptance target

--- windows/BCP_FINAL_ACCEPTANCE_CURRENT.ps1
+++ windows/BCP_FINAL_ACCEPTANCE_CURRENT.ps1
@@ -7,7 +7,7 @@ param(
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 
-$TargetVersion = "0.5.0"
+$TargetVersion = "0.6.0"
 $Project = "buildhub"
 $Port = 8765
 $BaseUrl = "http://127.0.0.1:$Port"
