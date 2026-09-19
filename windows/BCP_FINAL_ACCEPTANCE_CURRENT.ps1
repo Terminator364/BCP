9108c0f1fcd8e6e912e5fdc7a591423dcd4ff584
Terminator364 <158576221+Terminator364@users.noreply.github.com>
2026-09-19 04:16:20 +0100

    field: align future final acceptance with BCP 0.4.7

--- windows/BCP_FINAL_ACCEPTANCE_CURRENT.ps1
+++ windows/BCP_FINAL_ACCEPTANCE_CURRENT.ps1
@@ -7,7 +7,7 @@ param(
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 
-$TargetVersion = "0.4.6"
+$TargetVersion = "0.4.7"
 $Project = "buildhub"
 $Port = 8765
 $BaseUrl = "http://127.0.0.1:$Port"
