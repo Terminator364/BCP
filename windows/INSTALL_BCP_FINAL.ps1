ecbe4d71da9165e0c8756de6b571085a116847c7
Terminator364 <158576221+Terminator364@users.noreply.github.com>
2026-09-19 16:44:37 +0100

    Installer: align BCP package to 0.5.1

--- windows/INSTALL_BCP_FINAL.ps1
+++ windows/INSTALL_BCP_FINAL.ps1
@@ -7,7 +7,7 @@ param(
 
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
-$InstallerVersion = "0.5.0"
+$InstallerVersion = "0.5.1"
 $RuleName = "BCP Local LAN 8765"
 $Port = 8765
 $ScriptRoot = Split-Path -Parent $PSCommandPath
