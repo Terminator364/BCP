9243109a2d179b79f4e5fbf51c15aff55caff52a
Terminator364 <158576221+Terminator364@users.noreply.github.com>
2026-09-21 18:35:27 +0100

    R74 expose phone resource governor through node status API

--- android-b-edge/src/main/java/com/blessing/bcpedge/EdgeRelayService.java
+++ android-b-edge/src/main/java/com/blessing/bcpedge/EdgeRelayService.java
@@ -298,6 +298,7 @@ public final class EdgeRelayService extends Service {
             out.put("permissions", EdgePermissionManager.status(this));
             out.put("content_store", client.contentStoreStatus());
             out.put("network", EdgeNetworkState.snapshot(this));
+            out.put("resources", EdgeResourceGovernor.snapshot(this));
             out.put("pending_jobs",
                     EdgeDatabase.get(this).edgeDao().countPendingJobs());
             out.put("presence", presenceState);
